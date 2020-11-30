380ebd8e4f9579c154d442388f7642005e077833
JJDENG-work <69386247+JJDENG-work@users.noreply.github.com>
2020-11-30 15:31:10 +0800

    Update 113001继承子类.App.java

--- 113001继承子类.App.java
+++ 113001继承子类.App.java
@@ -6,7 +6,13 @@ public class App extends R{
 	public static void main(String[] args) {
 		System.out.println(R.boy_filePath);
 		System.out.println(R.girl_filePath);
-		System.out.println(R.array2d[1][1]);
+		
+		for(int row=0; row<R.array2d.length; row++){
+			for (int m=0; m < R.array2d[row].length; m++) {
+				System.out.print(R.array2d[row][m]+" ");
+			}
+			System.out.println();
+		}
 
 	}
 

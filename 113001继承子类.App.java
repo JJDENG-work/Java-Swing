7ecc417a9695c9f0369d0bad2786f5df9c1b1168
JJDENG-work <69386247+JJDENG-work@users.noreply.github.com>
2020-11-30 15:08:53 +0800

    Create 113001继承子类.App.java

---
+++ 113001继承子类.App.java
@@ -0,0 +1,13 @@
+package gks;
+
+public class App extends R{
+
+
+	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
+		System.out.println(R.array2d[1][1]);
+
+	}
+
+}

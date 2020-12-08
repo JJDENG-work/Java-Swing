25ccbea85e0c845a7a7596084eb4fc3c003cd92b
JJDENG-work <69386247+JJDENG-work@users.noreply.github.com>
2020-12-08 14:16:02 +0800

    Update 120701.寻找0的相邻坐标.Find.java

--- 120701.寻找0的相邻坐标.Find.java
+++ 120701.寻找0的相邻坐标.Find.java
@@ -1,10 +1,12 @@
 package com;
 
 public class Find {
+	public int X;
+	public int Y;
 
 	public void name(int arrar2d[][]) {
-		int x=arrar2d[0].length;
-		int y=arrar2d.length;
+		X=arrar2d[0].length;
+		Y=arrar2d.length;
 		for (int row = 0; row < arrar2d.length; row++) {
 			for (int column = 0; column < arrar2d[0].length; column++) {
 				if (arrar2d[row][column]==0) {
@@ -32,18 +34,18 @@ public class Find {
 				System.out.println("R_left("+x+","+(y-1)+")");
 			}
 		}
-		public void FindRight(int x, int y) {
-			if (y+1>5) {
-				System.out.println("R_right("+x+","+y+")");
-			}else {
-				System.out.println("R_right("+x+","+(y+1)+")");
-			}
-	}
 		public void FindDown(int x, int y) {
-			if (x+1>3) {
+			if (x+1>X) {
 				System.out.println("R_down("+x+","+y+")");
 			}else {
 				System.out.println("R_down("+(x+1)+","+y+")");
+			}
+	}
+		public void FindRight(int x, int y) {
+			if (x+1>Y) {
+				System.out.println("R_down("+x+","+y+")");
+			}else {
+				System.out.println("R_down("+x+","+(y+1)+")");
 			}
 	}
 		

31a5d2ad0af45bb31037cc272a81e469d02adc7e
JJDENG-work <69386247+JJDENG-work@users.noreply.github.com>
2020-12-14 14:44:25 +0800

    Create 121401奇数求和.App.java

---
+++ 121401奇数求和.App.java
@@ -0,0 +1,15 @@
+package gks;
+
+public class App {
+
+	public static void main(String[] args) {
+		long num=0;
+		for (int i = 1; i < 2147483647; i++) {
+			if (i%2==1) {
+				num=i+num;
+			}
+		}
+		System.out.println("1+3+5+7+...2147483647等于："+num);
+	}
+
+}
